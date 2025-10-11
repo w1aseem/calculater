df935a34dc469f4a4e6ca01604eab5f6c9d39e8b
a1rthur <waseemalsis73@gmail.com>
2025-10-11 15:50:43 +0300

    fixed code

--- project1/calculater.py
+++ project1/calculater.py
@@ -1,42 +1,80 @@
-def add(x,y):
-    return x+y 
-def subtract(x,y):
-    return x-y  
-def multiply(x,y):
-     return x*y
-def divide(x,y):
-    if y==0:
-     return "Error! Division by zero."
-    else:
-     return x/y
-def exponent(x,y):
-     return x**y
-def modulus(x,y):
-      return x%y
-def floor_divide(x,y):
-    if y==0:
-        return "Error! Division by zero."
-    else:
-            return x//y
-#single -input operations
 import math
 
-def square_root(x): 
-    if x<0:
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    else:
+        return x / y
+
+
+def exponent(x, y):
+    return x**y
+
+
+def modulus(x, y):
+    return x % y
+
+
+def floor_divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    else:
+        return x // y
+
+
+# single -input operations
+
+
+def square_root(x):
+    if x < 0:
         return "cannot take square root of negative number"
     else:
-            return math.sqrt(x)
+        return math.sqrt(x)
+
 
 def log10(x):
     if x <= 0:
         return "logarithm undefined for non-positive numbers"
     else:
         return math.log10(x)
-        
+
+
 def natural_log(x):
-    if x<=0:
+    if x <= 0:
 
         return "natural log undefined for non -positive numbers"
-    
+
     else:
         return math.log(x)
+
+
+def sine(x):
+    return math.sin(math.radians(x))
+
+
+def cosine(x):
+    return math.cos(math.radians(x))
+
+
+def tangent(x):
+    return math.tan(math.radians(x))
+
+
+def factorial(x):
+    if x < 0:
+        return "factorial not defined for negative numbers"
+    else:
+        return math.factorial(x)
